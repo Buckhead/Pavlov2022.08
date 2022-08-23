07b0119a942dd476306be576ff55edf1b4a83c67
Buckhead.Korea <buckhead.korea@gmail.com>
2022-08-24 06:44:31 +0900

    Project started

--- app/src/main/java/com/buckheadkorea/pavlov/client202208/FragmentA.kt
+++ app/src/main/java/com/buckheadkorea/pavlov/client202208/FragmentA.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [MainPage.newInstance] factory method to
+ * Use the [FragmentA.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MainPage : Fragment() {
+class FragmentA : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class MainPage : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_page, container, false)
+        return inflater.inflate(R.layout.fragment_a, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class MainPage : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment MainPage.
+         * @return A new instance of fragment FragmentA.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            MainPage().apply {
+            FragmentA().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
--- app/src/main/java/com/buckheadkorea/pavlov/client202208/MainActivity.kt
+++ app/src/main/java/com/buckheadkorea/pavlov/client202208/MainActivity.kt
@@ -2,10 +2,30 @@ package com.buckheadkorea.pavlov.client202208
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.buckheadkorea.pavlov.client202208.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+//        프래그먼트를 생성
+        val fragmentList = listOf(FragmentA(),FragmentB(),FragmentC(),FragmentD())
+
+        //어댑터를 생성하고, 생성해둔 프래그먼트 목록을 저장한다.
+        val adapter = FragmentAdapter(this)
+        adapter.fragmentList = fragmentList
+
+        //뷰페이저에 어댑터를 연결해준다.
+        binding.viewPager.adapter = adapter
+
+        //TabLayout과 뷰 페이저를 연결한다.
+        // TabLayoutMediator로 TabLayout과 뷰페이저를 연결한다.
+        val tabTitles = listOf<String>("A","B","C","D")
+        TabLayoutMediator(binding.tabLayout, binding.viewPager){ tab, position ->
+            tab.text = tabTitles[position]
+        }.attach()
+
     }
 }
